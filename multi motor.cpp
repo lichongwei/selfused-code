18f1041df6fc7bb6144398aaf43e3be871940c13
lichongwei <35536028+lichongwei@users.noreply.github.com>
2018-02-14 13:46:49 +0000

    Add files via upload

---
+++ multi motor.cpp
@@ -0,0 +1,67 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <wiringPi.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int sensorin1;
+	int sensorin2;
+	int sensorin3;
+	wiringPiSetup();
+	pinMode(1, OUTPUT);
+	pinMode(2, OUTPUT);
+	pinMode(3, OUTPUT);
+	for (int counter = 0; counter<20; counter++)
+	{
+
+
+		 int state;
+		 state = 1 * sensorin1 + 2 * sensorin2 + 4 * sensorin3;
+	//Motor 1
+			 if (state == 1 or state ==3 or state == 5 or state == 7)
+			 {
+				 digitalWrite(1, 1);
+				 cout << "on" << endl;
+				 delay(100)
+
+					 digitalWrite(1, 0);
+				 cout << "off" << endl;
+				 delay(100);
+			 }
+	
+	//Motor 2
+			 if (state == 2 or state == 3 or state == 6 or state == 7)
+			 {
+				 digitalWrite(2, 1);
+				 cout << "on" << endl;
+				 delay(100)
+
+					 digitalWrite(2, 0);
+				 cout << "off" << endl;
+				 delay(100);
+			 }
+	//Motor 3
+			if (state == 4 or state == 5 or state == 6 or state == 7)
+			{
+				digitalWrite(3, 1);
+				cout << "on" << endl;
+				delay(100)
+
+					digitalWrite(3, 0);
+				cout << "off" << endl;
+				delay(100);
+			}
+
+
+
+	}
+
+	return 0;
+
+}
+
